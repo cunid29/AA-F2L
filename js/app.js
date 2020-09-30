cae707c8f68d1e381a3dc9c56822a66fd45dad2d
Nguyễn Văn Nhân <vannhan292000@gmail.com>
2020-09-30 17:49:27 +0700

    master

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
-angular.module('myApp', ['ngRoute'])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/home', {
-                templateUrl: '../pages/home.html'
-            })
-            .when('/our-story', {
-                templateUrl: '../pages/our-story.html'
-            })
-            .when('/investors', {
-                templateUrl: '../pages/investors.html'
-            })
-            .when('/media', {
-                templateUrl: '../pages/media.html'
-            })
-            .when('/contact', {
-                templateUrl: '../pages/contact.html'
-            })
-            .when('/product', {
-                templateUrl: '../pages/product.html'
-            })
-            .when('/career', {
-                templateUrl: '../pages/career.html'
-            })
-            .when('/solution', {
-                templateUrl: '../pages/solution.html'
-            })
-            .when('/member', {
-                templateUrl: '../pages/member.html'
-            })
-            .when('/member/register', {
-                templateUrl: '../pages/member-register.html',
-            })
-            .when('/investors/register', {
-                templateUrl: '../pages/investors-register.html',
-            })
-            .otherwise({
-                redirectTo: '/home'
-            });
-    });
+var app = angular.module('myApp', ['ngRoute'])
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../pages/home.html'
+        })
+        .when('/our-story', {
+            templateUrl: '../pages/our-story.html'
+        })
+        .when('/investors', {
+            templateUrl: '../pages/investors.html'
+        })
+        .when('/media', {
+            templateUrl: '../pages/media.html'
+        })
+        .when('/contact', {
+            templateUrl: '../pages/contact.html'
+        })
+        .when('/product', {
+            templateUrl: '../pages/product.html'
+        })
+        .when('/career', {
+            templateUrl: '../pages/career.html'
+        })
+        .when('/solution', {
+            templateUrl: '../pages/solution.html'
+        })
+        .when('/member', {
+            templateUrl: '../pages/member.html'
+        })
+        .when('/member/register', {
+            templateUrl: '../pages/member-register.html',
+        })
+        .when('/investors/register', {
+            templateUrl: '../pages/investors-register.html',
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+});
