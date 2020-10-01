ea055b34a6870e3bfc4e9afd5a6745c32b697c18
Nguyễn Văn Nhân <vannhan292000@gmail.com>
2020-10-01 13:45:46 +0700

    master

--- js/app.js
+++ js/app.js
@@ -1,63 +1,64 @@
-var app = angular.module('myApp', ['ngRoute'])
-app.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: '../pages/home.html'
-        })
-        .when('/our-story', {
-            templateUrl: '../pages/our-story.html'
-        })
-        .when('/investors', {
-            templateUrl: '../pages/investors.html'
-        })
-        .when('/media', {
-            templateUrl: '../pages/media.html'
-        })
-        .when('/contact', {
-            templateUrl: '../pages/contact.html'
-        })
-        .when('/product', {
-            templateUrl: '../pages/product.html'
-        })
-        .when('/career', {
-            templateUrl: '../pages/career.html'
-        })
-        .when('/solution', {
-            templateUrl: '../pages/solution.html'
-        })
-        .when('/member', {
-            templateUrl: '../pages/member.html'
-        })
-        .when('/member/register', {
-            templateUrl: '../pages/member-register.html',
-        })
-        .when('/investors/register', {
-            templateUrl: '../pages/investors-register.html',
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
-});
-app.filter('cut', function() {
-    return function(value, wordwise, max, tail) {
-        if (!value) return '';
+    // Single Page Application
+    var app = angular.module('myApp', ['ngRoute'])
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: '../pages/home.html'
+            })
+            .when('/our-story', {
+                templateUrl: '../pages/our-story.html'
+            })
+            .when('/investors', {
+                templateUrl: '../pages/investors.html'
+            })
+            .when('/media', {
+                templateUrl: '../pages/media.html'
+            })
+            .when('/contact', {
+                templateUrl: '../pages/contact.html'
+            })
+            .when('/product', {
+                templateUrl: '../pages/product.html'
+            })
+            .when('/career', {
+                templateUrl: '../pages/career.html'
+            })
+            .when('/solution', {
+                templateUrl: '../pages/solution.html'
+            })
+            .when('/member', {
+                templateUrl: '../pages/member.html'
+            })
+            .when('/member/register', {
+                templateUrl: '../pages/member-register.html',
+            })
+            .when('/investors/register', {
+                templateUrl: '../pages/investors-register.html',
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+    //Filter String
+    app.filter('cut', function() {
+        return function(value, wordwise, max, tail) {
+            if (!value) return '';
 
-        max = parseInt(max, 10);
-        if (!max) return value;
-        if (value.length <= max) return value;
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
 
-        value = value.substr(0, max);
-        if (wordwise) {
-            var lastspace = value.lastIndexOf(' ');
-            if (lastspace !== -1) {
-                //Also remove . and , so its gives a cleaner result.
-                if (value.charAt(lastspace - 1) === '.' || value.charAt(lastspace - 1) === ',') {
-                    lastspace = lastspace - 1;
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace !== -1) {
+                    if (value.charAt(lastspace - 1) === '.' || value.charAt(lastspace - 1) === ',') {
+                        lastspace = lastspace - 1;
+                    }
+                    value = value.substr(0, lastspace);
                 }
-                value = value.substr(0, lastspace);
             }
-        }
 
-        return value + (tail || ' …');
-    };
-});
+            return value + (tail || ' …');
+        };
+    });
